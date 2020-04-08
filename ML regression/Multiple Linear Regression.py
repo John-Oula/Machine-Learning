8b085d62fbcfe4c68360a8ec41103104f433a6a3
John-Oula <johnoula@icloud.com>
2020-04-09 03:30:57 +0800

    MLR

--- ML regression/Multiple Linear Regression.py
+++ ML regression/Multiple Linear Regression.py
@@ -4,20 +4,21 @@ import pandas as pd
 
 # Importing the dataset
 
-dataset = pd.read_csv("Data.csv")
+dataset = pd.read_csv("50_Startups.csv")
 
 X = dataset.iloc[:, :-1].values  # independent variables
-y = dataset.iloc[:, 3].values  # dependent variable
+y = dataset.iloc[:, 4].values  # dependent variable
 
 #Encoding categorical data
 from sklearn.preprocessing import LabelEncoder ,OneHotEncoder
 labelencoder_X=LabelEncoder()
-X[:,0] = labelencoder_X.fit_transform(X[:,0])
-onehotencoder = OneHotEncoder(categorical_features=[0]) #pre-specified the array column
+X[:,3] = labelencoder_X.fit_transform(X[:,3])
+onehotencoder = OneHotEncoder(categorical_features=[3]) #pre-specified the array column
 X = onehotencoder.fit_transform(X).toarray()
 
-labelencoder_y=LabelEncoder()
-y = labelencoder_y.fit_transform(y)
+#Avoiding the dummy variable trap
+#X=X[:,1:]""""""
+
 
 # Splitting the dataset into the Training set and Test set
 
@@ -31,3 +32,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""""
+
+#Fitting MLR to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+#Predicting
+y_pred = regressor.predict(X_test)
